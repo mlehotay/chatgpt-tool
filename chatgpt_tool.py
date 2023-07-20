3e7a1908364911c7cc732b311e4cf47de77b3119
Michael Lehotay <mlehotay@gmail.com>
2023-07-20 16:54:00 -0400

    fix messy arg parsing code

--- chatgpt_tool.py
+++ chatgpt_tool.py
@@ -9,109 +9,30 @@ import zipfile
 """
 ChatGPT Tool
 
-ChatGPT Tool is a command-line utility for importing ChatGPT conversations from the ChatGPT export JSON format and managing them in a SQLite database.
+ChatGPT Tool is a command-line utility for importing ChatGPT conversations from JSON and managing them in a SQLite database.
 
-See README.md for more information.
 """
 
 class ChatGPTTool:
     DEFAULT_DB_NAME = "chatgpt.db"
-    DEFAULT_DATA_DIRECTORY = "data"
+    DEFAULT_DATA_PATH = "data"
 
-# init
+    # init
+    ###########################################################################
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description="ChatGPT Tool")
-        self.subparsers = self.parser.add_subparsers(title="subcommands", dest="subcommand")
+        self.parser = argparse.ArgumentParser(prog="chatgpt_tool", description="ChatGPT Tool")
+        self.parser.add_argument("-db", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="Database name")
+        self.parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+        self.parser.add_argument("subcommand", choices=["import", "show", "delete", "info", "help", "export", "conversation", "inspect"],
+                                 nargs="?", default="help", help="Subcommand")
+        self.parser.add_argument("path", nargs="?", default=self.DEFAULT_DATA_PATH, help="Filename or directory for import (default: data)")
 
-        self.setup_import_command()
-        self.setup_print_command()
-        self.setup_delete_command()
-        self.setup_help_command()
-        self.setup_info_command()
-        self.setup_test_command()
-        self.setup_export_command()
-        self.setup_conversation_command()
-        self.setup_inspect_command()
-
-# setup subcommands
-
-    def setup_import_command(self):
-        # Create the 'import' subcommand parser
-        import_parser = self.subparsers.add_parser("import", help="Import JSON data into the SQLite database")
-        import_parser.add_argument("-d", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="SQLite database name")
-        import_parser.add_argument("-f", "--data-directory", type=str, default=self.DEFAULT_DATA_DIRECTORY, help="Data directory to import (JSON format)")
-
-    def setup_print_command(self):
-        # Create the 'print' subcommand parser
-        print_parser = self.subparsers.add_parser("print", help="Print the content of the SQLite database tables")
-        print_parser.add_argument("-d", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="SQLite database name")
-
-    def setup_delete_command(self):
-        # Create the 'delete' subcommand parser
-        delete_parser = self.subparsers.add_parser("delete", help="Delete the SQLite database file")
-        delete_parser.add_argument("-d", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="SQLite database name")
-
-    def setup_help_command(self):
-        # Create the 'help' subcommand parser
-        help_parser = self.subparsers.add_parser("help", help="Show usage information for the tool")
-        help_subparsers = help_parser.add_subparsers(title="subcommands", dest="subcommand")
-
-        # Add subparsers for each subcommand
-        import_help_parser = help_subparsers.add_parser("import", help="Show usage information for the import subcommand")
-        print_help_parser = help_subparsers.add_parser("print", help="Show usage information for the print subcommand")
-        delete_help_parser = help_subparsers.add_parser("delete", help="Show usage information for the delete subcommand")
-        help_help_parser = help_subparsers.add_parser("help", help="Show usage information for the help subcommand")
-        info_help_parser = help_subparsers.add_parser("info", help="Show usage information for the info subcommand")
-        test_help_parser = help_subparsers.add_parser("test", help="Show usage information for the test subcommand")
-
-    def setup_info_command(self):
-        # Create the 'info' subcommand parser
-        info_parser = self.subparsers.add_parser("info", help="Display statistics about the database and the data")
-        info_parser.add_argument("-d", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="SQLite database name")
-
-    def setup_test_command(self):
-        # Create the 'test' subcommand parser
-        test_parser = self.subparsers.add_parser("test", help="Run the doctests")
-        test_parser.add_argument("-v", "--verbose", action="store_true", help="Display verbose output")
-
-# import
-
-    def setup_info_command(self):
-        # Create the 'info' subcommand parser
-        info_parser = self.subparsers.add_parser("info", help="Display statistics about the database and the data")
+    # import
+    ###########################################################################
 
     def import_data(self, db_name, data_directory=None):
-        """
-        Import JSON data into the SQLite database.
-
-        >>> # Test import with a valid JSON file
-        >>> tool = ChatGPTTool()
-        >>> tool.import_data("test.db", "data.json")
-        Importing data from: data.json
-
-        >>> # Test import with a non-existent data directory
-        >>> tool = ChatGPTTool()
-        >>> tool.import_data("test.db", "nonexistent")
-        Error: Data directory not found: nonexistent
-
-        >>> # Test import with an empty data directory
-        >>> tool = ChatGPTTool()
-        >>> tool.import_data("test.db", "empty_directory")
-        Error: No JSON data files found.
-
-        >>> # Test import with an invalid zip file
-        >>> tool = ChatGPTTool()
-        >>> tool.import_data("test.db", "invalid_zip.zip")
-        Error: Invalid zip file: invalid_zip.zip
-
-        >>> # Test import with a zip file containing no JSON files
-        >>> tool = ChatGPTTool()
-        >>> tool.import_data("test.db", "empty_zip.zip")
-        Error: No JSON data files found in the zip file.
-        """
-        data_directory = data_directory or self.DEFAULT_DATA_DIRECTORY
-
+        data_directory = data_directory or self.DEFAULT_DATA_PATH
         if not os.path.exists(data_directory):
             print(f"Error: Data directory not found: {data_directory}")
             return
@@ -155,32 +76,7 @@ class ChatGPTTool:
         conn.close()
 
     def get_data_files(self, data_directory=None):
-        """
-        Get a list of data files in the specified directory.
-
-        >>> # Test with a valid data directory containing JSON files
-        >>> tool = ChatGPTTool()
-        >>> tool.get_data_files("data")
-        ['data/data1.json', 'data/data2.json']
-
-        >>> # Test with a non-existent data directory
-        >>> tool = ChatGPTTool()
-        >>> tool.get_data_files("nonexistent")
-        Error: Data directory not found: nonexistent
-        []
-
-        >>> # Test with a zip file containing JSON files
-        >>> tool = ChatGPTTool()
-        >>> tool.get_data_files("data.zip")
-        ['data.zip/data1.json', 'data.zip/data2.json']
-
-        >>> # Test with a zip file containing no JSON files
-        >>> tool = ChatGPTTool()
-        >>> tool.get_data_files("empty_zip.zip")
-        Error: No JSON data files found in the zip file.
-        []
-        """
-        data_directory = data_directory or self.DEFAULT_DATA_DIRECTORY
+        data_directory = data_directory or self.DEFAULT_DATA_PATH
 
         if not os.path.exists(data_directory):
             print(f"Error: Data directory not found: {data_directory}")
@@ -204,18 +100,6 @@ class ChatGPTTool:
         return data_files
 
     def import_json_data_to_sqlite(self, cursor, table_name, json_data):
-        """
-        Import JSON data into SQLite database.
-
-        >>> # Test import with a valid JSON data
-        >>> tool = ChatGPTTool()
-        >>> cursor = None  # Mock cursor object
-        >>> json_data = [
-        ...     {"id": 1, "name": "John Doe"},
-        ...     {"id": 2, "name": "Jane Smith"}
-        ... ]
-        >>> tool.import_json_data_to_sqlite(cursor, "data_1", json_data)
-        """
         # Check if the JSON data is empty
         if not json_data:
             print(f"Skipping import for table: {table_name} (empty JSON data)")
@@ -263,30 +147,10 @@ class ChatGPTTool:
             insert_query = f"INSERT OR IGNORE INTO {table_name} ({','.join(json_data.keys())}) VALUES ({','.join(['?'] * len(values))})"
             cursor.execute(insert_query, values)
 
-# print
+    # show
+    ###########################################################################
 
     def print_tables(self, db_name):
-        """
-        Print the content of the SQLite database tables.
-
-        >>> # Test printing tables with a valid database
-        >>> tool = ChatGPTTool()
-        >>> tool.print_tables("test.db")
-        Table: table1
-        ['id', 'name']
-        ('1', 'John Doe')
-        ('2', 'Jane Smith')
-
-        Table: table2
-        ['id', 'message']
-        ('1', 'Hello')
-        ('2', 'Hi')
-
-        >>> # Test printing tables with a non-existent database
-        >>> tool = ChatGPTTool()
-        >>> tool.print_tables("nonexistent.db")
-        Error: Database not found: nonexistent.db
-        """
         conn = sqlite3.connect(db_name)
         cursor = conn.cursor()
 
@@ -312,7 +176,8 @@ class ChatGPTTool:
 
         conn.close()
 
-# delete
+    # delete
+    ###########################################################################
 
     def delete_database(self, db_name):
         try:
@@ -321,7 +186,8 @@ class ChatGPTTool:
         except FileNotFoundError:
             print(f"Database not found: {db_name}")
 
-# info
+    # info
+    ###########################################################################
 
     def info(self, db_name):
         # Connect to the database
@@ -349,20 +215,8 @@ class ChatGPTTool:
         # Close the database connection
         conn.close()
 
-# test
-
-    def test(self, verbose):
-        if verbose:
-            print("Running doctests...")
-        doctest.testmod(verbose=verbose)
-
-# export
-
-    def setup_export_command(self):
-        # Create the 'export' subcommand parser
-        export_parser = self.subparsers.add_parser("export", help="Export conversations from the SQLite database to JSON format")
-        export_parser.add_argument("-d", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="SQLite database name")
-        export_parser.add_argument("-o", "--output-directory", type=str, default=self.DEFAULT_DATA_DIRECTORY, help="Output directory to export conversations (JSON format)")
+    # export
+    ###########################################################################
 
     def export_conversations(self, db_name, output_directory=None):
         output_directory = output_directory or self.DEFAULT_DATA_DIRECTORY
@@ -401,14 +255,8 @@ class ChatGPTTool:
 
         return json_data
 
-# conversation
-
-    def setup_conversation_command(self):
-        # Create the 'conversation' subcommand parser
-        conversation_parser = self.subparsers.add_parser("conversation", help="Print a conversation from the SQLite database")
-        conversation_parser.add_argument("-d", "--db-name", type=str, default=self.DEFAULT_DB_NAME, help="SQLite database name")
-        conversation_parser.add_argument("-t", "--table", type=str, required=True, help="Table name containing the conversation")
-        conversation_parser.add_argument("-i", "--id", type=str, required=True, help="ID of the conversation")
+    # conversation
+    ###########################################################################
 
     def print_conversation(self, db_name, table_name, conversation_id):
         conn = sqlite3.connect(db_name)
@@ -429,12 +277,8 @@ class ChatGPTTool:
 
         conn.close()
 
-# inspect
-
-    def setup_inspect_command(self):
-        # Create the 'inspect' subcommand parser
-        inspect_parser = self.subparsers.add_parser("inspect", help="Inspect data files before importing")
-        inspect_parser.add_argument("-f", "--data-directory", type=str, default=self.DEFAULT_DATA_DIRECTORY, help="Data directory to inspect (JSON format)")
+    # inspect
+    ###########################################################################
 
     def inspect_data(self, data_directory=None):
         data_directory = data_directory or self.DEFAULT_DATA_DIRECTORY
@@ -480,7 +324,8 @@ class ChatGPTTool:
         print(f"Number of Records: {len(json_data)}")
         # ...
 
-# utility functions
+    # utility functions
+    ###########################################################################
 
     def truncate_string(self, string, max_length):
         if len(string) <= max_length:
@@ -491,7 +336,8 @@ class ChatGPTTool:
         terminal_size = shutil.get_terminal_size(fallback=(80, 24))
         return terminal_size.columns - 3  # Subtract 3 to account for ellipsis
 
-# database functions
+    # database functions
+    ###########################################################################
 
     def get_table_names(self, cursor):
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -501,23 +347,17 @@ class ChatGPTTool:
         cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
         return cursor.fetchone()[0]
 
-# run
+    # run
+    ###########################################################################
 
     def run(self):
-        """
-        Run the ChatGPT tool.
-
-        >>> # Test running the tool
-        >>> tool = ChatGPTTool()
-        >>> tool.run()
-        """
         args = self.parser.parse_args()
 
         if args.subcommand == "import":
             print("Action: Import")
             self.import_data(args.db_name, args.data_directory)
-        elif args.subcommand == "print":
-            print("Action: Print tables")
+        elif args.subcommand == "show":
+            print("Action: Display database tables")
             self.print_tables(args.db_name)
         elif args.subcommand == "delete":
             print("Action: Delete database")
@@ -528,18 +368,22 @@ class ChatGPTTool:
         elif args.subcommand == "help":
             print("Action: Display help")
             self.parser.print_help()
-        elif args.subcommand == "test":
-            print("Action: Run doctests")
-            self.test(args.verbose)
+        elif args.subcommand == "export":
+            print("Action: Export conversations")
+            self.export_conversations(args.db_name, args.data_directory)
+        elif args.subcommand == "conversation":
+            print("Action: Print conversation")
+            self.print_conversation(args.db_name, args.table_name, args.conversation_id)
         elif args.subcommand == "inspect":
             print("Action: Inspect data files")
             self.inspect_data(args.data_directory)
         else:
-            # No subcommand specified, try to import using default settings
-            print("Action: Import using default settings")
-            self.import_data(self.DEFAULT_DB_NAME, None)
+            # No subcommand specified, display usage information
+            print("Action: None")
+            self.parser.print_help()
 
 # main
+###############################################################################
 
 if __name__ == "__main__":
     tool = ChatGPTTool()
